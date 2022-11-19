6c930d84bbf7481c6c8a0a9e7b699bbe9bb8b5e0
Angelina <38373766+filangelin@users.noreply.github.com>
2022-11-19 20:04:55 +0300

    Update test_petstore.py

--- test_petsore/test_petstore.py
+++ test_petsore/test_petstore.py
@@ -10,3 +10,9 @@ def test_check_response():
     url = "https://petstore.swagger.io/v2/pet/11"
     response=requests.get(url).json()
     assert response["name"]=="No, my doggie"
+
+@pytest.mark.parametrize("key, value, id", [("name", "No, my doggie", 11), ("status", "available", 11)])
+def test_piece_of_body(key, value, id):
+    url = f"https://petstore.swagger.io/v2/pet/{11}"
+    response=requests.get(url).json()
+    assert response[key]==value
